f561f3c3eda2f243169d19ab0b886979e7e36b12
Jeffrey Yeh <jeffreyyehuw@gmail.com>
2023-05-25 14:00:11 -0700

    something appears

---
+++ my-app/src/components/pages/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>
+        Email address: jeffreyyehuw@gmail.com
+      </p>
+      <p>
+        Phone Number: 425.233.1650 
+      </p>
+    </div>
+  );
+}
