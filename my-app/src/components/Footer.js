8b39c75911870b7ed9b37c59193daa28183f2c1e
Jeffrey Yeh <jeffreyyehuw@gmail.com>
2023-05-25 19:27:41 -0700

    implemented footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from 'react-icons/ai';
 
 export default function Footer() {
     return (
       <div>
-        {/* TODO: get logo to appear on webpage, then add LinkedIn + other social media */}
+        {/* Footer displays logos/links to GitHub, LinkedIn, and YouTube profiles */}
         <a href="https://github.com/ETFruitNinja/">
-            <img src={require ("../github-mark.png")} alt="GitHub Logo" />
+            <AiFillGithub/>
+        </a>
+        <a href="https://www.linkedin.com/in/jeffrey-yeh-9a3928166/">
+            <AiFillLinkedin/>
+        </a>
+        <a href="https://www.youtube.com/channel/UCuc2d6DM-LYEypFZPCLtB6Q">
+            <AiFillYoutube/>
         </a>
       </div>
     );
